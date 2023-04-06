c467fb276d523785b611cdcfe0d6500c05cf166a
IlaMuniz <ilammuniz@gmail.com>
2023-04-06 13:26:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Front End Dinâmico
+
+### Programa Santander Coders 2022
+___
+Trilha - Automação de Testes
+
+Módulo VI: Front End Dinâmico
+___
+### Atividade avaliativa do módulo
+
+**Instruções:**
+
+1- Pegar o projeto base da consulta por CEP
+
+2- Resolver o bug do cartão indefinido
+
+3- Resolver o bug do cartão consultar
+
+4- Ao consultar um endereço válido, armazenar no localstorage e exibir todos os cartões
---
+++ cep.js
@@ -0,0 +1,67 @@
+console.log("=== CEP ===");
+
+// Actions
+
+function onlyNumbers() {
+    // console.log(this value, this.value.match(/\d+/));
+    // console.log(this.value, /\d+/.test(this.value));
+    this.value = this.value.replace(/\D+/g, "");
+}
+
+function validateEntry() {
+    this.value.length;
+    if (this.value.length === 8) {
+        // this.style.borderColor = "";
+        // this.style.borderWidth = "";
+        // this.style.backgroundColor = "";
+        this.classList.remove("error");
+        getAddress(this.value);
+    } else {
+        // this.style.borderColor = "red";
+        // this.style.borderWidth = "2px";
+        // this.style.backgroundColor = "yellow";
+        this.classList.add("error");
+        this.focus();
+    }
+}
+
+function getAddress(postalCode) {
+    console.log("get address", arguments[0], postalCode);
+
+    // endpoint
+    const endpoint = `https://viacep.com.br/ws/${postalCode}/json/`;
+    console.log(endpoint);
+
+    // config
+    const config = {
+        method: "GET"
+    };
+
+    // request
+    fetch(endpoint, config)
+        .then(function(resp) { return resp.json(); })
+        .then(getAddressSuccess)
+        .catch(getAddressError);
+}
+
+function getAddressSuccess(address) {
+    const { logradouro, cep, localidade, uf, bairro } = address;
+
+    const card = `<div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                        <h5 class="card-title">${logradouro}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${bairro} - ${localidade} - ${uf}</h6>
+                        <p class="card-text">${cep}</p>
+                    </div>
+                </div>`
+
+    document.querySelector(".cards").innerHTML = card;
+}
+
+function getAddressError() {
+    console.log("deu ruim 1!");
+}
+
+// Mapping Events
+document.querySelector("#cep").addEventListener("input", onlyNumbers);
+document.querySelector("#cep").addEventListener("focusout", validateEntry);
